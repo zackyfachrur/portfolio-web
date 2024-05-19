bb53e8f17fc76bc8d9bc2b71d8096e17d1066fc6
zackyfachrur <mochzackyaja@gmail.com>
2024-05-19 21:17:29 +0700

    add about content

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,28 +1,52 @@
 import "../tailwind.css";
 import { FaXmark, FaWhatsapp } from "react-icons/fa6";
 import { useState } from "react";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 
 const MessageWhatsapp = () => {
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
   if (name === "" || email === "" || message === "") {
-    return <NullOfInput />;
+    Swal.fire({
+      title: "Information",
+      text: "Messages Cannot Be Empty!",
+      confirmButtonColor: "#34403A",
+      confirmButtonText: "Done",
+      buttonsStyling: false,
+      customClass: {
+        confirmButton:
+          "bg-custom-green-dark px-5 text-white font-bold hover:scale-95 animate border-2 border-custom-green-dark rounded-full w-20 hover:border-custom-green-dark hover:drop-shadow-md",
+      },
+    });
   } else {
-    window.open(
-      `https://wa.me/6287703577988?text=From ${name}%0A${email}%0A%0A${message}`
-    );
+    Swal.fire({
+      title: "Information",
+      text: "Do you want to sent it?",
+      showCancelButton: true,
+      cancelButtonText: "No",
+      showDenyButton: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      confirmButtonText: "Yes",
+      buttonsStyling: false,
+      customClass: {
+        confirmButton:
+          "bg-custom-green-dark px-5 text-white font-bold hover:scale-95 animate border-2 border-custom-green-dark rounded-full w-20 hover:border-custom-green-dark hover:drop-shadow-md mr-2 flex cursor-pointer",
+        cancelButton:
+          "bg-custom-green-dark px-5 text-white font-bold hover:scale-95 animate border-2 border-custom-green-dark rounded-full w-20  hover:border-custom-green-dark hover:drop-shadow-md ml-2 flex cursor-pointer",
+      },
+    }).then((result) => {
+      if (result.isDismissed) {
+        return;
+      } else {
+        window.open(
+          `https://wa.me/6287703577988?text=From ${name}%0A${email}%0A%0A${message}`
+        );
+      }
+    });
   }
-};
-
-const NullOfInput = () => {
-  return (
-    <>
-      <div className="fixed flex flex-col gap-5 p-10 font-bold text-white rounded-lg bg-custom-green-dark text-start w-96 right-20 bottom-10">
-        <h1>Hello World</h1>
-      </div>
-    </>
-  );
 };
 
 const MessageButton = () => {
@@ -50,7 +74,6 @@ const ContactForm = () => {
     <form
       data-aos="fade"
       data-aos-duration="600"
-      onSubmit={MessageWhatsapp}
       className="fixed flex flex-col gap-5 p-10 font-bold text-white rounded-lg bg-custom-green-dark text-start w-96 right-20 bottom-10"
     >
       <h1 className="flex-row gap-4 pb-6 text-xl font-bold border-b-2 center">
@@ -78,7 +101,8 @@ const ContactForm = () => {
         />
       </div>
       <button
-        type="submit"
+        type="button"
+        onClick={MessageWhatsapp}
         className="self-center w-32 px-3 py-1 text-sm font-bold bg-white rounded-full text-custom-green-dark hover:scale-95 animate drop-shadow-2xl"
       >
         Submit
--- src/pages/Portfolio/PortfolioHome.jsx
+++ src/pages/Portfolio/PortfolioHome.jsx
@@ -6,54 +6,66 @@ import PringAstula from "../../assets/pringastula/dekstop-home.png";
 const PortfolioHome = () => {
   return (
     <>
-      <div className="flex flex-row items-center justify-center w-full h-96 gap-52">
-        <div className="center w-80">
-          <img src={Wilies} className="scale-150 rounded animate" alt="" />
-        </div>
-        <div className="flex-col gap-2 font-bold center w-72">
-          <h1 className="text-xl font-bold">Market Profile</h1>
-          <h2 className="text-custom-green-dark">Front-end Web</h2>
-          <p className="text-sm">
-            This website was created using the React JS library with a Slider in
-            it and has a form feature that has been validated so that the form
-            cannot be filled in if it {`doesn't`} have a value.
-          </p>
-          <button
-            className="w-32 px-3 py-1 text-sm border-2 rounded-full border-custom-green-dark bg-custom-green-dark hover:bg-custom-green-light hover:text-custom-green-dark animate"
-            onClick={() =>
-              window.open("https://wilies-shoes.web.app/", "_blank")
-            }
-          >
-            Preview Web
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-row-reverse items-center justify-center w-full h-96 gap-52 bg-custom-green-dark">
-        <div className="center w-80">
-          <img
-            src={MasakanMaman}
-            className="scale-150 rounded animate"
-            alt=""
-          />
-        </div>
-        <div className="flex-col gap-2 font-bold center w-72 ">
-          <h1 className="text-xl font-bold">Restaurant Profile</h1>
-          <h2 className="text-custom-green-dark">Front-end Web</h2>
-          <p className="text-sm">
-            This website was created using the React JS library with a Slider in
-            it and has a form feature that has been validated so that the form
-            cannot be filled in if it {`doesn't`} have a value.
-          </p>
-          <button
-            className="w-32 px-3 py-1 text-sm rounded-full border-custom-green-dark hover:scale-95 animate bg-custom-green-light text-custom-green-dark animate"
-            onClick={() =>
-              window.open("https://burger-web-page.vercel.app/", "_blank")
-            }
-          >
-            Preview Web
-          </button>
+      {/* Section Wilies */}
+
+      <div className="center">
+        <div className="flex flex-row items-center justify-center w-full h-96 gap-52">
+          <div className="center w-80">
+            <img src={Wilies} className="scale-150 rounded animate" alt="" />
+          </div>
+          <div className="flex-col gap-2 font-bold center w-72">
+            <h1 className="text-xl font-bold">Market Profile</h1>
+            <h2 className="text-custom-green-dark">Front-end Web</h2>
+            <p className="text-sm">
+              This website was created using the React JS library with a Slider
+              in it and has a form feature that has been validated so that the
+              form cannot be filled in if it {`doesn't`} have a value.
+            </p>
+            <button
+              className="w-32 px-3 py-1 text-sm border-2 rounded-full border-custom-green-dark bg-custom-green-dark hover:bg-custom-green-light hover:text-custom-green-dark animate"
+              onClick={() =>
+                window.open("https://wilies-shoes.web.app/", "_blank")
+              }
+            >
+              Preview Web
+            </button>
+          </div>
         </div>
       </div>
+      {/* End Section Wilies */}
+
+      {/* Section Masakan Maman */}
+      <div className="flex justify-end align-items-end ">
+        <div className="flex flex-row-reverse items-center justify-center w-full max-w-screen-lg h-96 gap-52 bg-custom-green-dark rounded-l-full">
+          <div className="center w-80">
+            <img
+              src={MasakanMaman}
+              className="scale-150 rounded animate"
+              alt=""
+            />
+          </div>
+          <div className="flex-col gap-2 font-bold center w-72 ">
+            <h1 className="text-xl font-bold">Restaurant Profile</h1>
+            <h2 className="text-custom-green-darker">Front-end Web</h2>
+            <p className="text-sm">
+              This website was created using the React JS library with a Slider
+              in it and has a form feature that has been validated so that the
+              form cannot be filled in if it {`doesn't`} have a value.
+            </p>
+            <button
+              className="w-32 px-3 py-1 text-sm rounded-full border-custom-green-dark hover:scale-95 animate bg-custom-green-light text-custom-green-dark animate"
+              onClick={() =>
+                window.open("https://burger-web-page.vercel.app/", "_blank")
+              }
+            >
+              Preview Web
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* End Section Masakan Maman  */}
+
+      {/* Section Astula */}
       <div className="flex flex-row items-center justify-center w-full h-96 gap-52">
         <div className="center w-80">
           <img src={PringAstula} className="scale-150 rounded animate" alt="" />
@@ -76,6 +88,7 @@ const PortfolioHome = () => {
           </button>
         </div>
       </div>
+      {/* End Section Astula */}
     </>
   );
 };
